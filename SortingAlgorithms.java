45908a2a4b452cb5446257a56429d7630a414966
Elegade <46755428+Elegade@users.noreply.github.com>
2021-01-30 23:03:51 -0500

    Fixing test

--- SortingAlgorithms.java
+++ SortingAlgorithms.java
@@ -8,7 +8,6 @@ import java.util.Scanner;
  * SortingAlgorithms
  */
 public class SortingAlgorithms {
-    test
     public static void main(String[] args) {
         int size;
         Random rand = new Random();
